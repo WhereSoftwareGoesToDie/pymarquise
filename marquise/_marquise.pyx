c2c3fa216b0d3c959ace78cbc576a57a7e7a8a6f
Sharif Olorin <sio@tesser.org>
2014-04-14 13:30:44 +1000

    Fix C types

--- marquise/_marquise.pyx
+++ marquise/_marquise.pyx
@@ -1,5 +1,7 @@
 cimport cmarquise
 
+from errors import MarquiseException
+
 cdef class Marquise:
 	cdef cmarquise.marquise_consumer _c_consumer
 	cdef cmarquise.marquise_connection _c_connection
@@ -7,5 +9,9 @@ cdef class Marquise:
 		cdef bytes broker_bytes = broker.encode()
 		cdef char* c_broker = broker_bytes
 		cdef double c_batch_period = batch_period
-		_c_consumer = cmarquise.marquise_consumer_new(c_broker, c_batch_period)
-		_c_connection = cmarquise.marquise_connect(_c_consumer)
+		self._c_consumer = cmarquise.marquise_consumer_new(c_broker, c_batch_period)
+		if self._c_consumer is NULL:
+			raise MarquiseException("marquise_consumer_new returned NULL")
+		self._c_connection = cmarquise.marquise_connect(self._c_consumer)
+		if self._c_connection is NULL:
+			raise MarquiseException("marquise_connect returned NULL")
