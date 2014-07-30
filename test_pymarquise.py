ee3b57e679acc6863dc30f63ecab8eb4e736b3b6
Barney Desmond <barney.desmond@anchor.net.au>
2014-07-30 13:55:13 +1000

    Huge shakedown of the testsuite for awesomeness
    This is too big and messy to easily split over several commits, but the
    gist is that tests are now better grouped together, and I've added
    linting to satisfy myself that the testsuite is fairly clean and sanely
    done.
    
    * Has pylint directives that should give a 10/10 score
    * Docstrings up the wazoo on everything
    * Some name changes to keep pylint happy
    * More consistent input being tested in the send/update functions
    * send_{simple,extended} now also tested for write-failure conditions
    * Testing for good behaviour if user attempts to write-after-close
    * Test that double-closes are handled properly

--- test_pymarquise.py
+++ test_pymarquise.py
@@ -1,7 +1,20 @@
+# pylint: disable=line-too-long
+# pylint: disable=bad-whitespace
+# I just like to watch the world burn:
+# pylint: disable=mixed-indentation
+# pylint: disable=invalid-name
+
+"""This should be a comprehensive testsuite for Pymarquise's functionality."""
+
 import sys
 import os
 import pytest
 from marquise import Marquise
+
+# This keeps pylint happy
+# pylint: disable=no-member
+RAISES = pytest.raises
+# pylint: enable=no-member
 
 TEST_GOOD_NAMESPACE     = "mytestnamespace"
 TEST_BAD_NAMESPACE      = "3v1l_L33T_BaD_nAmEsPaCe!!!!!!"
@@ -26,133 +39,168 @@ DEBUG = False
 
 
 def test_yo_dawg_i_heard_you_liek_tests():
+	"""Ensure that the "bad" input to another test function is indeed bad enough."""
 	assert len(TEST_TOO_LONG_NAMESPACE) > 256
 
 def test_hash_identifier():
+	"""Ensure that we can call hash_identifier and get the right answer back."""
 	assert Marquise.hash_identifier(TEST_IDENTIFIER) == 7602883380529707052
 
 def test_bogus_namespace():
-	with pytest.raises(ValueError):
-		m = Marquise(TEST_BAD_NAMESPACE, debug=DEBUG)
+	"""Ensure that invalid namespaces are not accepted."""
+	with RAISES(ValueError):
+		# pylint: disable=unused-variable
+		marq = Marquise(TEST_BAD_NAMESPACE, debug=DEBUG)
+		# pylint: enable=unused-variable
 
 def test_very_very_very_long_namespace():
-	with pytest.raises(RuntimeError) as excinfo:
-		m = Marquise(TEST_TOO_LONG_NAMESPACE, debug=DEBUG)
+	"""Ensure that a too-long namespace is not accepted."""
+	with RAISES(RuntimeError) as excinfo:
+		# pylint: disable=unused-variable
+		marq = Marquise(TEST_TOO_LONG_NAMESPACE, debug=DEBUG)
+		# pylint: enable=unused-variable
 	assert excinfo.value.args[0].endswith("errno is 36")
 
 def test_print_a_marquise_with_debugging():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=True)
-	print(m)
-	m.close()
-
+	"""Ensure that the Marquise class' __str__ method doesn't explode."""
+	print("-"*72)
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=True)
+	print(marq)
+	marq.close()
+	print("-"*72)
 
 
 
 def test_send_simple():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	"""Exercise send_simple with good and bad input."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
 	# Normal use specifying address directly
-	assert m.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
-	assert m.send_simple(address=TEST_GOOD_ADDRESS, timestamp=1234567890, value=42)
-	# Non-simple value
-	with pytest.raises(TypeError):
-		m.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value="pantsu")
+	assert marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
+	assert marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=1234567890, value=42)
+	# Non-numeric and not-None timestamp
+	with RAISES(TypeError):
+		assert marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp="pantsu", value=42)
+	# Non-numeric value
+	with RAISES(TypeError):
+		marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value="pantsu")
 	# Non-numeric address
-	with pytest.raises(TypeError):
-		m.send_simple(address=TEST_SOURCE1, timestamp=None, value=42)
-	m.close()
+	with RAISES(TypeError):
+		marq.send_simple(address=TEST_SOURCE1, timestamp=None, value=42)
+	# None as a value
+	with RAISES(TypeError):
+		assert marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=None)
+	marq.close()
 
 
 def test_send_simple_after_close():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	m.close()
-	with pytest.raises(ValueError):
-		m.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
-	assert m.close() is None
+	"""Ensure that send_simple explodes if you attempt to use a closed handle."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	marq.close()
+	with RAISES(ValueError):
+		marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
+	assert marq.close() is None
 
 
-def test_how_do_i_even_call_send_simple_omg():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	with pytest.raises(TypeError):
-		m.send_simple(source="pantsu", timestamp=None, value=None)
-	with pytest.raises(TypeError):
-		m.send_simple(source=None, address=None, timestamp=None, value="pantsu")
-	with pytest.raises(TypeError):
-		m.send_simple(source="polka dots", address="stripes", timestamp=None, value="pantsu")
-	m.close()
-
-
+def test_send_simple_write_failure():
+	"""Ensure that send_simple explodes if the write fails."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	# Cause the write to fail due to the file being read-only.
+	os.chmod(marq.spool_path_points, 0o400)
+	with RAISES(RuntimeError) as excinfo:
+		marq.send_simple(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
+	assert excinfo.value.args[0].endswith("errno is 13")
+	marq.close()
 
 
 def test_send_extended():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	assert m.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value="lorem ipsum")
-	assert m.send_extended(address=TEST_GOOD_ADDRESS, timestamp=1234567890, value="dolor")
-	assert m.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value="dolorite")
-	assert m.send_extended(address=TEST_GOOD_ADDRESS, timestamp=9001, value="I love me some geology")
-	m.close()
-	with pytest.raises(ValueError):
-		m.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value="This is a closed handle.")
-	assert m.close() is None
+	"""Exercise send_simple with good and bad input."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	# Normal use specifying address directly
+	assert marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value="lorem ipsum")
+	assert marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp=1234567890, value="dolor")
+	# Non-numeric and not-None timestamp
+	with RAISES(TypeError):
+		assert marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp="pantsu", value="dolorite")
+	# Non-numeric address
+	with RAISES(TypeError):
+		assert marq.send_extended(address=TEST_SOURCE1, timestamp=None, value="I love me some geology")
+	# None as a value
+	with RAISES(TypeError):
+		assert marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value=None)
+	marq.close()
 
 
-def test_i_cannot_even_send_extended_wtf():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	with pytest.raises(TypeError):
-		m.send_extended(source="pantsu", timestamp=None, value=None)
-	with pytest.raises(TypeError):
-		m.send_extended(source=None, address=None, timestamp=None, value="pantsu")
-	with pytest.raises(TypeError):
-		m.send_extended(source="polka dots", address="stripes", timestamp=None, value="pantsu")
-	m.close()
+def test_send_extended_after_close():
+	"""Ensure that send_extended explodes if you attempt to use a closed handle."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	marq.close()
+	with RAISES(ValueError):
+		marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value="This is a closed handle.")
+	assert marq.close() is None
+
+
+def test_send_extended_write_failure():
+	"""Ensure that send_extended explodes if the write fails."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	# Cause the write to fail due to the file being read-only.
+	os.chmod(marq.spool_path_points, 0o400)
+	with RAISES(RuntimeError) as excinfo:
+		marq.send_extended(address=TEST_GOOD_ADDRESS, timestamp=None, value=42)
+	assert excinfo.value.args[0].endswith("errno is 13")
+	marq.close()
 
 
 def test_update_source():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	assert m.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
-	with pytest.raises(TypeError):
-		m.update_source(TEST_BAD_ADDRESS, TEST_GOOD_SOURCE_DICT)
+	"""Exercise update_source with good and bad input."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	# Normal use
+	assert marq.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
+	# Non-numeric address
+	with RAISES(TypeError):
+		marq.update_source(TEST_BAD_ADDRESS, TEST_GOOD_SOURCE_DICT)
+	# Keys and values set to None
+	with RAISES(TypeError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_NONE_KEY)
+	with RAISES(TypeError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_NONE_VAL)
+	# Keys and values set to Exception, a Python class that can't be cast to anything C
+	with RAISES(TypeError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_EXC_KEY)
+	with RAISES(TypeError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_EXC_VAL)
+	# Keys and values containing colons, which are invalid
+	with RAISES(ValueError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_COLON_KEY)
+	with RAISES(ValueError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_COLON_VAL)
+	marq.close()
 
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_NONE_KEY)
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_NONE_VAL)
 
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_EXC_KEY)
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_EXC_VAL)
-
-	with pytest.raises(ValueError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_COLON_KEY)
-	with pytest.raises(ValueError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_BAD_SOURCE_DICT_COLON_VAL)
-
-	# Cleanly close.
-	m.close()
-	# Detonate it.
-	with pytest.raises(ValueError):
-		m.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
-	# Double-close is safe.
-	assert m.close() is None
+def test_update_source_after_close():
+	"""Ensure that update_source explodes if you attempt to use a closed handle."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	marq.close()
+	with RAISES(ValueError):
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
+	assert marq.close() is None
 
 
 def test_update_source_write_failure():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	"""Ensure that update_source explodes if the write fails."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
 	# Cause the write to fail due to the file being read-only.
-	os.chmod(m.spool_path_contents, 0o400)
-	with pytest.raises(RuntimeError) as excinfo:
-		m.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
+	os.chmod(marq.spool_path_contents, 0o400)
+	with RAISES(RuntimeError) as excinfo:
+		marq.update_source(TEST_GOOD_ADDRESS, TEST_GOOD_SOURCE_DICT)
 	assert excinfo.value.args[0].endswith("errno is 13")
-	m.close()
+	marq.close()
 
 
-def test_i_have_no_idea_what_i_am_doing_with_update_source():
-	m = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_SOURCE_DICT, source=None, address=None)
-	with pytest.raises(TypeError):
-		m.update_source(TEST_GOOD_SOURCE_DICT, source="polka dots", address="stripes")
-	m.close()
+def test_double_close_okay():
+	"""Ensure that double-close() is safe, it should be a no-op."""
+	marq = Marquise(TEST_GOOD_NAMESPACE, debug=DEBUG)
+	marq.close()
+	assert marq.close() is None
 
 
 
@@ -165,13 +213,19 @@ if __name__ == '__main__':
 	test_bogus_namespace()
 	test_very_very_very_long_namespace()
 	test_print_a_marquise_with_debugging()
+
 	test_send_simple()
 	test_send_simple_after_close()
-	test_how_do_i_even_call_send_simple_omg()
+	test_send_simple_write_failure()
+
 	test_send_extended()
-	test_i_cannot_even_send_extended_wtf()
+	test_send_extended_after_close()
+	test_send_extended_write_failure()
+
 	test_update_source()
+	test_update_source_after_close()
 	test_update_source_write_failure()
-	test_i_have_no_idea_what_i_am_doing_with_update_source()
+
+	test_double_close_okay()
 
 	sys.exit(0)
