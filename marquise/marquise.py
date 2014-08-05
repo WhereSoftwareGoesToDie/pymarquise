80a11349624070bcb8d27aef4883f089bdc73bd0
Barney Desmond <barney.desmond@anchor.net.au>
2014-08-05 13:04:50 +1000

    Fix string formatting of `address` in debug output
    There's no guarantees that address is a number, the test coverage suite
    showed this manifesting when certain expected breakage wasn't occurring.
    
    Explicitly expecting a number (%d) was causing an exception to be thrown
    early, meaning we don't hit the case where bad input is fed to
    marquise_update_source() in the C code. Print as a string (%s) instead.
    
    I've left other instances of %d intact, where the value is guaranteed to
    only ever be an int (FFI.errno and the C return values).

--- marquise/marquise.py
+++ marquise/marquise.py
@@ -115,7 +115,7 @@ class Marquise(object):
         if self.marquise_ctx is None:
             raise ValueError("Attempted to write to a closed Marquise handle.")
 
-        self.__debug("Supplied address: %d" % address)
+        self.__debug("Supplied address: %s" % address)
 
         if value is None:
             raise TypeError("Can't store None as a value.")
@@ -149,7 +149,7 @@ class Marquise(object):
         if self.marquise_ctx is None:
             raise ValueError("Attempted to write to a closed Marquise handle.")
 
-        self.__debug("Supplied address: %d" % address)
+        self.__debug("Supplied address: %s" % address)
 
         if value is None:
             raise TypeError("Can't store None as a value.")
@@ -187,7 +187,7 @@ class Marquise(object):
         if self.marquise_ctx is None:
             raise ValueError("Attempted to write to a closed Marquise handle.")
 
-        self.__debug("Supplied address: %d" % address)
+        self.__debug("Supplied address: %s" % address)
 
         # Sanity check the input, everything must be UTF8 strings (not
         # yet confirmed), no Nonetypes or anything stupid like that.
